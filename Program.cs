5b56b58badd05210a6f40754ab6e9c9af7f2c2a8
Julia <wuwara@mail.ru>
2022-12-17 09:40:34 +0300

    изменения в 38 задаче

--- Program.cs
+++ Program.cs
@@ -124,113 +124,113 @@
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-// double[] CreateArray(int num, int min, int max)
-// {
-//     double[] array = new double[num];
-//     for (int i = 0; i < num; i++)
-//     {
-//         array[i] = Convert.ToDouble(new Random().Next(min, max));
+double[] CreateArray(int num, int min, int max)
+{
+    double[] array = new double[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().NextDouble()*100;
        
-//     }
-//     return array;
-// }
+    }
+    return array;
+}
 
-// void PrintArray(double[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-// }
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
 
-// double Min(double[] array)
-// {
-//     double min = array[0];
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (min > array[i])
-//         {
-//             min = array[i];
-//         }
-//     }
+double Min(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+    }
 
-//     return min;
-// }
+    return min;
+}
 
-// double Max(double[] array)
-// {
-//     double max = array[0];
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (max < array[i])
-//         {
-//             max = array[i];
-//         }
-//     }
-//     return max;
-// }
+double Max(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
 
 
-// double DifferenceMinMax(double Min, double Max)
-// {
-//     double difference = Max - Min;
-//     return difference;
-// }
+double DifferenceMinMax(double Min, double Max)
+{
+    double difference = Max - Min;
+    return difference;
+}
 
-// int num = 10;
-// int min = 0;
-// int max = 125;
+int num = 10;
+int min = 0;
+int max = 125;
 
-// double[] myArray = CreateArray(num, min, max);
-// PrintArray(myArray);
-// Console.WriteLine();
-// Console.WriteLine($"min = {Min(myArray)}");
-// Console.WriteLine($"max = {Max(myArray)}");
-// Console.WriteLine($"Разница между максимальным и минимальным значением массива равна {DifferenceMinMax(Min(myArray), Max(myArray))}");
+double[] myArray = CreateArray(num, min, max);
+PrintArray(myArray);
+Console.WriteLine();
+Console.WriteLine($"min = {Min(myArray)}");
+Console.WriteLine($"max = {Max(myArray)}");
+Console.WriteLine($"Разница между максимальным и минимальным значением массива равна {DifferenceMinMax(Min(myArray), Max(myArray))}");
 
 
 // Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
 
- int[] CreateArray(int num, int min, int max){
-      int[] array = new int[num];
-      for(int i = 0; i < num; i++){
-        array[i] = new Random().Next(min,max);
-      }
-      return array;   
-    }
+//  int[] CreateArray(int num, int min, int max){
+//       int[] array = new int[num];
+//       for(int i = 0; i < num; i++){
+//         array[i] = new Random().Next(min,max);
+//       }
+//       return array;   
+//     }
   
-    void ShowArray(int[] array){
-      for(int i = 0; i < array.Length; i++){
-        Console.Write($"{array[i]} ");
-      }
-    }
+//     void ShowArray(int[] array){
+//       for(int i = 0; i < array.Length; i++){
+//         Console.Write($"{array[i]} ");
+//       }
+//     }
 
 
-    int[] ResultArray(int[] array){  
-      int number = array.Length/2;
-      if(array.Length/2!=0){
-        number = array.Length/2+1;
-      }
-      int[] result = new int[number];
-      for(int i = 0; i < number; i++){
-        result[i] = array[i]*array[array.Length-i-1];
-        if(i == array.Length-i-1){
-          result[i] = array[i];
-        }
-      }
-      return result;
-    }
+//     int[] ResultArray(int[] array){  
+//       int number = array.Length/2;
+//       if(array.Length/2!=0){
+//         number = array.Length/2+1;
+//       }
+//       int[] result = new int[number];
+//       for(int i = 0; i < number; i++){
+//         result[i] = array[i]*array[array.Length-i-1];
+//         if(i == array.Length-i-1){
+//           result[i] = array[i];
+//         }
+//       }
+//       return result;
+//     }
     
 
 
 
-  int num = 5;
-  int min = -10;
-  int max = 10;
-  int[] myArray = CreateArray(num, min, max);
-  ShowArray(myArray);
-  Console.WriteLine();
-  int[] result = ResultArray(myArray);
-  ShowArray(result);
+//   int num = 5;
+//   int min = -10;
+//   int max = 10;
+//   int[] myArray = CreateArray(num, min, max);
+//   ShowArray(myArray);
+//   Console.WriteLine();
+//   int[] result = ResultArray(myArray);
+//   ShowArray(result);
